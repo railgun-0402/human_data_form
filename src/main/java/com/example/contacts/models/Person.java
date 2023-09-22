c1edb93e7b876512f30db7a7ba11afc687b7c6c6
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-09-22 15:13:31 +0900

    validation

--- src/main/java/com/example/contacts/models/Person.java
+++ src/main/java/com/example/contacts/models/Person.java
@@ -1,5 +1,12 @@
 package com.example.contacts.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +14,17 @@ import lombok.Setter;
 @Setter
 
 public class Person {
+    @NotBlank
+    @Size(max = 120)
     private String name;
+
+    @NotNull
+    @Min(0)
+    @Max(120)
     private Integer age;
+
+    @NotBlank
+    @Email
+    @Size(max = 254)
     private String email;
 }
